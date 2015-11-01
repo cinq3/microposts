d7ca62673b06705d3fd6e401696e57872867f799
cinq3 <cinqmars@nifty.com>
2015-11-01 02:38:52 +0000

    Lesson7-3rd

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 20151026161014) do
-ActiveRecord::Schema.define(version: 20151028005134) do
+ActiveRecord::Schema.define(version: 20151027173402) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
